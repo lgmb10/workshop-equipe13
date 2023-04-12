1557b332d7a4ef33279cf9fa6bdb1d88738bdec3
Yann-vdv <61906860+Yann-vdv@users.noreply.github.com>
2023-04-12 18:47:07 +0200

    game

--- public/js/index.js
+++ public/js/index.js
@@ -3,68 +3,225 @@ import walk from "../sprites/player/walk.mjs";
 import run from "../sprites/player/run.mjs";
 import hurt from "../sprites/player/hurt.mjs";
 import death from "../sprites/player/death.mjs";
-import { animate, clear } from "./sprite.mjs";
+import BACKGROUND from "../sprites/background.mjs";
+import { animate, clear, handleBackGround } from "./sprite.mjs";
+import { createItem, drawItem, isColision } from "./item.mjs";
 
-var canvas = document.querySelector('canvas');
-var context = canvas.getContext('2d');
+const gameArea = document.querySelector('.game-area');
+const hearts = document.querySelectorAll(".heart");
+const scoreDiv = document.querySelector(".score");
+
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
 context.fillStyle = 'white'
 
-var posIdle = 1;
-var posWalk = 1;
-var posRun = 1;
-var posHurt = 1;
-var posDeath = 1;
-var playerSizeMultiplier = 2.5
+let score = 0;
+let posIdle = 1;
+let posWalk = 1;
+let posRun = 1;
+let posHurt = 1;
+let posDeath = 1;
+let playerSizeMultiplier = 2.5
+let gameSpeed = 4;
+let time = 0.0;
+let gameOnGoing = false;
+let isAlive = true;
+let isHurt = false;
+let itemsList = [];
+let health = 3;
+let playerPeace = "idle";
+let slowed = false;
+let slowedValue = 0;
+let slowStartTime = 0;
 
-function player() {
-    posIdle >= idle.length && (posIdle = 0);
-    animate(posIdle,idle,{x:0,y:100},context,playerSizeMultiplier);
-    posWalk >= walk.length && (posWalk = 0);
-    animate(posWalk,walk,{x:100,y:100},context,playerSizeMultiplier);
-    posRun >= run.length && (posRun = 0);
-    animate(posRun,run,{x:200,y:100},context,playerSizeMultiplier);
-    posHurt >= hurt.length && (posHurt = 0);
-    animate(posHurt,hurt,{x:300,y:100},context,playerSizeMultiplier);
-    posDeath >= death.length && (posDeath = 0);
-    animate(posDeath,death,{x:400,y:100},context,playerSizeMultiplier);
+function handlePlayer() {
+    if (!gameOnGoing) {
+        if (isAlive) {
+            posIdle >= idle.length && (posIdle = 0);
+            animate(posIdle,idle,{x:50,y:100},context,playerSizeMultiplier);
+        } else {
+            posDeath >= death.length && (posDeath = death.length - 1);
+            animate(posDeath,death,{x:50,y:100},context,playerSizeMultiplier);
+        }
+    } else {
+        if (isHurt) {
+            posHurt >= hurt.length && (posHurt = 0,isHurt = false);
+            animate(posHurt,hurt,{x:50,y:100},context,playerSizeMultiplier);
+        } else {
+            if (gameSpeed >= 6) {
+                posRun >= run.length && (posRun = 0);
+                animate(posRun,run,{x:50,y:100},context,playerSizeMultiplier);
+            } else {
+                posWalk >= walk.length && (posWalk = 0);
+                animate(posWalk,walk,{x:50,y:100},context,playerSizeMultiplier);
+            }
+        }
+    }
 }
 
-var backSpeed = 2;
-var backImg = new Image();
-backImg.src = "../images/background/City1.png";
-const BG = {
-    x1: 0,
-    x2: canvas.width,
-    y: 0,
-    width: canvas.width,
-    height: canvas.height
+function updateHealth() {
+    for (let i = 0; i < hearts.length; i++) {
+        if (i >= health) {
+            hearts[i].classList.add("heartEmpty");
+        } else {
+            hearts[i].classList.remove("heartEmpty");
+        }
+    }
 }
 
-function handleBackGround() {
-    if (BG.x1 <= -BG.width + backSpeed) BG.x1 = BG.width;
-    else BG.x1 -= backSpeed;
-    if (BG.x2 <= -BG.width + backSpeed) BG.x2 = BG.width;
-    else BG.x2 -= backSpeed;
-    context.drawImage(backImg, BG.x1, BG.y, BG.width, BG.height);
-    context.drawImage(backImg, BG.x2, BG.y, BG.width, BG.height)
+function handleItems() {
+    if (itemsList.length > 0) {
+        if (itemsList[0].x <= -itemsList[0].width) {      //remove item when out of screen
+            score += 1;
+            scoreDiv.innerHTML = score;
+            itemsList.shift();
+        }
+        itemsList.forEach(item => {
+            gameOnGoing && (item.x -= gameSpeed);
+            if (item.jumping) {
+                if (item.jumpWay == 'up' && item.y > 10) {      //monte
+                    item.y -= gameSpeed*1.5;
+                } else if (item.jumpWay == 'up' && item.y < 10) {   //commence décente
+                    item.y += gameSpeed*1.2;
+                    item.jumpWay = 'down';
+                } else if (item.jumpWay == 'down') {     //décent
+                    if (item.y < 135) {
+                        item.y += gameSpeed*1.2;
+                    } else {                        //attérit
+                        item.y = 135;
+                        item.jumpWay = 'up';
+                        item.jumping = false;
+                    }
+                }
+            } 
+            
+            drawItem(item,{x:item.x,y:item.y},context);
+
+            if (!isHurt && isColision(item,playerPeace)) {
+                if (item.type == 'box') {
+                    if (health > 1) {
+                        isHurt = true;
+                    } else {
+                        isAlive = false;
+                        gameOnGoing = false;
+                    }
+                    health--;
+                } else if (item.type == 'heart' && health < 3) {
+                    health++
+                } 
+                // else if (item.type == 'reduceSpeed') {
+                //     if (!slowed) {
+                //         slowedValue = gameSpeed * 0.3;
+                //         gameSpeed = gameSpeed * 0,7;
+                //         slowed = true;
+                //     }
+                //     slowStartTime = time;
+                // } 
+                else if (item.type == 'coin') {
+                    score += 5;
+                    scoreDiv.innerHTML = score;
+                }
+                updateHealth();
+                itemsList.shift();
+            }
+        });
+    }
 }
 
-setInterval(() => {
-    posIdle++;
-    posWalk++;
-    posRun++;
-    posHurt++;
-    posDeath++;
-}, 120);
+document.addEventListener('keydown', (e) => {
+    if (e.code == "Space") {
+        if (gameOnGoing) {
+            itemsList.every(item => {
+                if (!item.jumping) {
+                    item.jumping = true;
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+        } else if (isAlive) {
+            posIdle = 1;
+            gameOnGoing = true;
+            playerPeace = "walk";
+        }
+    }
+});
+
+
+function start() {
+    setInterval(() => {
+        if (!gameOnGoing) {
+            if (isAlive) {
+                posIdle++;
+            } else if (posIdle < death.length) {
+                posDeath++;
+            }
+        } else {
+            if (isHurt) {
+                posHurt++;
+            } else {
+                if (gameSpeed >= 6) {
+                    posRun++;
+                } else {
+                    posWalk++;
+                }
+            }
+        }
+    }, 120);
+    setInterval(() => {
+        game();
+    }, 20);
+    setInterval(() => {
+        if (gameOnGoing) {
+            time += 0.1;
+            const timeContainer = document.querySelector(".time");
+            timeContainer.innerHTML = `temps : ${time.toFixed(1)} s`;
+        }
+    }, 100)
+    let interval = Math.round(Math.random()*2000 + 200);
+    function itemsInterval() {
+        clearInterval(items)
+        if (gameOnGoing) {
+            let newItem = createItem(gameArea.offsetWidth,health);
+            itemsList.push(newItem);
+        }
+        interval = Math.round(Math.random()*2000 + 200);
+        items = setInterval(() => {
+            itemsInterval();
+        }, interval);
+    }
+    let items = setInterval(() => {
+        itemsInterval();
+    }, interval);
+    setInterval(() => {
+        gameSpeed += 0.5;
+    }, 15000)
+    function getRandBackground() {
+        return Math.floor((Math.random()*4) + 1)
+    }
+    setInterval(() => {
+        if (gameOnGoing) {
+            let newId = getRandBackground();
+            while (newId == BACKGROUND.id) {
+                newId = getRandBackground()
+            }
+            BACKGROUND.image.src = `../images/background/City${newId}.png`;
+            BACKGROUND.id = newId;
+        }
+    }, 30000)
+    // setInterval(() => {
+    //     if (slowed && slowStartTime + 5 <= time) {
+    //         slowed = false;
+    //         gameSpeed = gameSpeed + slowedValue;
+    //     }
+    // }, 1000)
+}
 
 function game() {
     clear(canvas,context);
-
-    handleBackGround();
-    player();
+    handleBackGround(BACKGROUND,gameOnGoing ? gameSpeed : 0,context);
+    handleItems();
+    handlePlayer();
 }
 
-setInterval(() => {
-    game();
-}, 20);
-
+start();
---
+++ public/sprites/background.mjs
@@ -0,0 +1,13 @@
+const BACKGROUND = {
+    x1: 0,
+    x2: 1774,
+    y: 0,
+    width: 1774,
+    height: 200,
+    image: new Image(),
+    id:1
+}
+
+BACKGROUND.image.src = "../images/background/City1.png";
+
+export default BACKGROUND;
